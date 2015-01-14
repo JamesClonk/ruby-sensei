b43dcc21e38ffc4f844053f234e329803d474ac5
JamesClonk <jamesclonk@jamesclonk.ch>
2015-01-14 16:47:55 +0100

    directories

--- 08_io/07_directories.rb
+++ 08_io/07_directories.rb
@@ -1,2 +1,31 @@
 # directories
 puts "\n***********************************\ndirectories"
+
+puts File.dirname(__FILE__)
+puts File.expand_path(File.dirname(__FILE__))
+
+puts Dir.pwd
+
+Dir.chdir("..")
+puts Dir.pwd
+
+Dir.chdir(File.join('08_io'))
+puts Dir.pwd
+
+puts Dir.entries(".").to_s
+
+Dir.entries(".").each do |entry|
+  print entry + ": "
+  if File.file?(entry) && File.readable?(entry)
+    File.open(entry, "r") do |file|
+      puts file.gets
+    end
+  else
+    puts
+  end
+end
+
+Dir.foreach(".") {|entry| puts entry}
+
+Dir.mkdir("temp_dir")
+Dir.delete("temp_dir")
