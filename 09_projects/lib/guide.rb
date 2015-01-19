7fec6b1122c7f7bc2200f56c6a4c112a443f6483
JamesClonk <jamesclonk@jamesclonk.ch>
2015-01-19 12:38:01 +0100

    add file creation

--- 09_projects/lib/guide.rb
+++ 09_projects/lib/guide.rb
@@ -1,6 +1,18 @@
+require 'restaurant'
+
 class Guide
 
   def initialize(path=nil)
+    Restaurant.filepath = path
+
+    if Restaurant.file_usable?
+      puts "Found restaurant file.\n\n"
+    elsif Restaurant.create_file
+      puts "Created restaurant file.\n\n"
+    else
+      puts "Exiting.\n\n"
+      exit!
+    end
   end
 
   def launch!
