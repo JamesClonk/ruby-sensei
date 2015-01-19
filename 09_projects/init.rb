0afe53421e0bdb1261d7ace43477fb7a3c33ac44
JamesClonk <jamesclonk@jamesclonk.ch>
2015-01-19 12:19:21 +0100

    app path

---
+++ 09_projects/init.rb
@@ -0,0 +1,8 @@
+# project example
+
+APP_ROOT = File.dirname(__FILE__)
+
+#require "#{APP_ROOT}/lib/guide.rb"
+#require File.join(APP_ROOT, "lib", "guide.rb")
+$:.unshift(File.join(APP_ROOT, 'lib'))
+require 'guide'
